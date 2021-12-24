bcdbb75477b048430b15f9afd557198826f43227
natalijapavlovic17321 <nnatanna@elfak.rs>
2021-12-24 02:04:22 +0100

    kom

---
+++ Faza2_17321_17080_17326/states.py
@@ -0,0 +1,18 @@
+from projekat1_17321_17080_17326 import main
+'''trenutno stanje sadrzi
+tabla=[] //tablu
+x1=()-ne treba //uvek isto za celu igru
+x2=()-ne treba //uvek isto za celu igru
+o1=()-ne treba //uvek isto za celu igru
+o2=()-ne treba //uvek isto za celu igru
+player1=True-ne treba //uvek isto za celu igru
+moved=False-mslm da ne treba/ promenljiva koja sluzi samo za pamcenje dal je odigran potez
+n=0-ne treba //uvek isto za celu igru
+m=0-ne treba //uvek isto za celu igru
+pozicije={} //pozicije igraca
+!!! pozicije zidova obavezno
+cijiPotez="x" //treba  - u sustini ne jer nam je potrebno samo za komp
+xZidovi=0  //treba
+oZidovi=0  //treba
+'''
+main()
