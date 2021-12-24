ebfedee1ea615db875c29b8443ab58b8b7fff632
Mateja <matejapancic@gmail.com>
2021-12-24 01:26:45 +0100

    UEA PARTIZAN

--- Faza1_17321_17080_17326/projekat1_17321_17080_17326.py
+++ Faza1_17321_17080_17326/projekat1_17321_17080_17326.py
@@ -156,6 +156,38 @@ def brojZidova():
             xZidovi=n-2
     oZidovi = xZidovi
     print(xZidovi)
+
+def findShortestPath(player, end):
+    path = []
+    queue = [[player]]
+    while queue:
+        curr, *queue = queue
+        pos = curr[-1]
+        if end[0] > pos[0] and end[1] > pos[1]:
+            nextPos = (pos[0] + 1, pos[1] + 1)
+        elif end[0] > pos[0] and end[1] < pos[1]:
+            nextPos = (pos[0] + 1, pos[1] - 1)
+        elif end[0] < pos[0] and end[1] > pos[1]:
+            nextPos = (pos[0] - 1, pos[1] + 1)
+        elif end[0] < pos[0] and end[1] < pos[1]:
+            nextPos = (pos[0] - 1, pos[1] - 1)
+        elif end[0] > pos[0]:
+            nextPos = (pos[0] + 1, pos[1])
+        elif end[0] < pos[0]:
+            nextPos = (pos[0] - 1, pos[1])
+        elif end[1] > pos[1]:
+            nextPos = (pos[0], pos[1] + 1)
+        else:
+            nextPos = (pos[0], pos[1] - 1)
+        if nextPos == end and (not path or len(curr) + 1 < len(path)):
+            path = curr + [end]
+        elif nextPos not in curr:
+            queue += [curr + [nextPos]]
+    return path
+
+
+print(findShortestPath((5, 5), (4, 3)))
+    
 def plavi():
     global tabla
     global n
