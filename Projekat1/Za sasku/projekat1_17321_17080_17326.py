182e2e9a49bbd9fe3e8714b309cab57ac17f3f5f
unknown <djokicaleksandra99@gmail.com>
2022-01-17 01:09:41 +0100

    sveto pismo

--- Projekat1/Za sasku/projekat1_17321_17080_17326.py
+++ Projekat1/Za sasku/projekat1_17321_17080_17326.py
@@ -1020,23 +1020,23 @@ def proceni_stanje2(stanj,igrac,moj_potez):
                         pozicijee[stanje[i][j]]=[i,j]
 
     for zid in zidovi:
-        if(zid[0]>=min(pozicijee["px1"][0],o1[0]) and zid[0]<=max(pozicijee["px1"][0],o1[0]) and zid[1]>=min(pozicijee["px1"][1],o1[1]) and zid[1]<=max(pozicijee["px1"][1],o1[1])):
-            score-=1
-        if(zid[0]>=min(pozicijee["px2"][0],o1[0]) and zid[0]<=max(pozicijee["px2"][0],o1[0]) and zid[1]>=min(pozicijee["px2"][1],o1[1]) and zid[1]<=max(pozicijee["px2"][1],o1[1])):
-            score-=1
-        if(zid[0]>=min(pozicijee["px1"][0],o2[0]) and zid[0]<=max(pozicijee["px1"][0],o2[0]) and zid[1]>=min(pozicijee["px1"][1],o2[1]) and zid[1]<=max(pozicijee["px1"][1],o2[1])):
-            score-=1
-        if(zid[0]>=min(pozicijee["px2"][0],o2[0]) and zid[0]<=max(pozicijee["px2"][0],o2[0]) and zid[1]>=min(pozicijee["px2"][1],o2[1]) and zid[1]<=max(pozicijee["px2"][1],o2[1])):
-            score-=1
 
-        if(zid[0]>=min(pozicijee["po1"][0],x1[0]) and zid[0]<=max(pozicijee["po1"][0],x1[0]) and zid[1]>=min(pozicijee["po1"][1],x1[1]) and zid[1]<=max(pozicijee["po1"][1],x1[1])):
-            score-=1
-        if(zid[0]>=min(pozicijee["po2"][0],x1[0]) and zid[0]<=max(pozicijee["po2"][0],x1[0]) and zid[1]>=min(pozicijee["po2"][1],x1[1]) and zid[1]<=max(pozicijee["po2"][1],x1[1])):
-            score-=1
-        if(zid[0]>=min(pozicijee["po1"][0],x2[0]) and zid[0]<=max(pozicijee["po1"][0],x2[0]) and zid[1]>=min(pozicijee["po1"][1],x2[1]) and zid[1]<=max(pozicijee["po1"][1],x2[1])):
-            score-=1
-        if(zid[0]>=min(pozicijee["po2"][0],x2[0]) and zid[0]<=max(pozicijee["po2"][0],x2[0]) and zid[1]>=min(pozicijee["po2"][1],x2[1]) and zid[1]<=max(pozicijee["po2"][1],x2[1])):
-            score-=1
+        if(zid[0]>=min(pozicijee["px1"][0],x1[0]) and zid[0]<=max(pozicijee["px1"][0],x1[0]) and zid[1]>=min(pozicijee["px1"][1],x1[1]) and zid[1]<=max(pozicijee["px1"][1],x1[1])):
+                score-=1
+        if(zid[0]>=min(pozicijee["px2"][0],x1[0]) and zid[0]<=max(pozicijee["px2"][0],x1[0]) and zid[1]>=min(pozicijee["px2"][1],x1[1]) and zid[1]<=max(pozicijee["px2"][1],x1[1])):
+                score-=1
+        if(zid[0]>=min(pozicijee["px1"][0],x2[0]) and zid[0]<=max(pozicijee["px1"][0],x2[0]) and zid[1]>=min(pozicijee["px1"][1],x2[1]) and zid[1]<=max(pozicijee["px1"][1],x2[1])):
+                score-=1
+        if(zid[0]>=min(pozicijee["px2"][0],x2[0]) and zid[0]<=max(pozicijee["px2"][0],x2[0]) and zid[1]>=min(pozicijee["px2"][1],x2[1]) and zid[1]<=max(pozicijee["px2"][1],x2[1])):
+                score-=1
+        if(zid[0]>=min(pozicijee["po1"][0],o1[0]) and zid[0]<=max(pozicijee["po1"][0],o1[0]) and zid[1]>=min(pozicijee["po1"][1],o1[1]) and zid[1]<=max(pozicijee["po1"][1],o1[1])):
+                score-=1
+        if(zid[0]>=min(pozicijee["po2"][0],o1[0]) and zid[0]<=max(pozicijee["po2"][0],o1[0]) and zid[1]>=min(pozicijee["po2"][1],o1[1]) and zid[1]<=max(pozicijee["po2"][1],o1[1])):
+                score-=1
+        if(zid[0]>=min(pozicijee["po1"][0],o2[0]) and zid[0]<=max(pozicijee["po1"][0],o2[0]) and zid[1]>=min(pozicijee["po1"][1],o2[1]) and zid[1]<=max(pozicijee["po1"][1],o2[1])):
+                score-=1
+        if(zid[0]>=min(pozicijee["po2"][0],o2[0]) and zid[0]<=max(pozicijee["po2"][0],o2[0]) and zid[1]>=min(pozicijee["po2"][1],o2[1]) and zid[1]<=max(pozicijee["po2"][1],o2[1])):
+                score-=1
     p1=617
     if  moj_potez:
         if(igrac=="x"):
@@ -1354,7 +1354,7 @@ def igraj():
             update()
             zid()
             update()
-            rez=minimax2(np.copy(tabla),1,True,(np.copy(tabla), 0),(np.copy(tabla), 617),None)
+            rez=minimax2(np.copy(tabla),1,True,(np.copy(tabla), -617),(np.copy(tabla), 617),None)
             naj=rez[0]
             tabla=np.copy(naj)
             if(oZidovi>0):
@@ -1373,7 +1373,7 @@ def igraj():
             elif  cijiPotez=="x":
                 cijiPotez="o"
         else:
-            rez=minimax2(np.copy(tabla),1,True,(np.copy(tabla), 0),(np.copy(tabla), 617),None)
+            rez=minimax2(np.copy(tabla),1,True,(np.copy(tabla), -617),(np.copy(tabla), 617),None)
             naj=rez[0]
             tabla=np.copy(naj)
             printT(tabla)
